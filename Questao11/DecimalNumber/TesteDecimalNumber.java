be390d07b410c3cc158fb37080ac2f9f8a5b1f01
Mcps4 <61793220+Mcps4@users.noreply.github.com>
2020-12-08 19:59:58 -0300

    Delete TesteDecimalNumber.java

--- Questao11/DecimalNumber/TesteDecimalNumber.java
+++
@@ -1,8 +0,0 @@
-public class TesteDecimalNumber {
-    public static void main(String[] args){
-        DecimalNumber decimalNumber = new DecimalNumber();
-        decimalNumber.number = 7.5;
-        System.out.println("Parte inteira: " + decimalNumber.NumeroInteiro());
-        System.out.println("Parte decimal: " + decimalNumber.NumeroDecimal());
-    }
-}
