f103d877a2119ce0465a1043c9a2af190eb3ba8c
Mcps4 <61793220+Mcps4@users.noreply.github.com>
2021-01-24 18:52:57 -0300

    Update TesteSenha.java

--- Atividade3/Questao3/TesteSenha.java
+++ Atividade3/Questao3/TesteSenha.java
@@ -5,13 +5,12 @@ public class TesteSenha {
 		Senha senha = new Senha();
 		
 		senha.valor = "Saladacozida123";
-		
 		senha.iguais("Saladacozida123");
 		senha.iguaisTrim(" Saladacozida123 ");
-		System.out.println(senha.tamanhoSeguro() ? "V." : "F.");
-		System.out.println(senha.possuiMaiusculaMinuscula() ? "V." : "F.");
-		System.out.println(senha.possuiNumero() ? "V." : "F.");
-		System.out.println(senha.ehValida() ? "V." : "F.");
+		System.out.println(senha.tamanhoSeguro());
+	    	System.out.println(senha.possuiNumero()");
+		System.out.println(senha.possuiMaiusculaMinuscula());
+	   	System.out.println(senha.ehValida()");
 		
 	}
 }
